6d3c2b40b4cd840eaa0a1b73e9167921578f672d
FlowerGrid <FlowerGridCoding@gmail.com>
2026-01-26 22:20:34 -0600

    Add label to hamburger menu

--- static/js/hamburgerMenu.js
+++ static/js/hamburgerMenu.js
@@ -1,12 +1,13 @@
 
 // Mobile styling hamburger menu
 // The Pizza King Forever Reigns
+const hamburgerContainer = document.querySelector('.hamburger-menu')
 const hamburger = document.querySelector('.hamburger');
 console.log('hamburger');
 const navLinks = document.querySelector('.nav-links');
 const mainContentDiv = document.querySelector('.main-content');
 
-hamburger.addEventListener('click', () => {
+hamburgerContainer.addEventListener('click', () => {
     navLinks.classList.toggle('open');
     hamburger.classList.toggle('open');
 })
